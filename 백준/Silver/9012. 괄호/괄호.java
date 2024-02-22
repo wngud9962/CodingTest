295094309d5ed235ca8e96736df6c63476416333
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-02-22 18:20:01 +0900

    Update 괄호.java

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -6,6 +6,10 @@ import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
+	//")"이 들어왔을때 stack이 비어있으면 여는괄호가 없기 때문에 NO
+	//반복종료
+	
+	//반복문이 끝나고 스택에 저장된게 없으면 YES
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,11 +24,13 @@ public class Main {
 			for(int j = 0; j < data.length(); j++) {
 				if(data.charAt(j) == '(') {
 					st.push(data.charAt(j));
-				}else {
+				}
+				else if(data.charAt(j) == ')'){
 					if(st.empty()) {
 						st.push(data.charAt(j));
 						break;
-					}else {
+					}
+					else {
 						st.pop();
 					}
 				}
