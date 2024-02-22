c36cb28ed4ff84837cb8fae5507db8dccff1e43f
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-02-22 19:00:11 +0900

    Update 괄호.java

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -6,46 +6,47 @@ import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
-	//")"이 들어왔을때 stack이 비어있으면 여는괄호가 없기 때문에 NO
-	//반복종료
-	
-	//반복문이 끝나고 스택에 저장된게 없으면 YES
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int line = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < line; i++) {
-			String data = br.readLine();
-			
-			Stack<Character> st = new Stack<Character>();
-			
-			for(int j = 0; j < data.length(); j++) {
-				if(data.charAt(j) == '(') {
-					st.push(data.charAt(j));
-				}
-				else if(data.charAt(j) == ')'){
-					if(st.empty()) {
-						st.push(data.charAt(j));
-						break;
-					}
-					else {
-						st.pop();
-					}
-				}
-			}
-			if(st.empty()) {
-				bw.write("YES");
-				bw.newLine();
-			}
-			else {
-				bw.write("NO");
-				bw.newLine();
-			}
-		}
-		br.close();
-		bw.close();
-	}
+    //")"이 들어왔을때 stack이 비어있으면 여는괄호가 없기 때문에 NO
+    //반복종료
+    
+    //반복문이 끝나고 스택에 저장된게 없으면 YES
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Character> st = new Stack<Character>();
+        
+        int line = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < line; i++) {
+        	st.clear();
+            String data = br.readLine();
+            
+            for(int j = 0; j < data.length(); j++) {
+                if(data.charAt(j) == '(') {
+                    st.push(data.charAt(j));
+                }
+                else if(data.charAt(j) == ')'){
+                    if(st.empty()) {
+                        st.push(data.charAt(j));
+                        break;
+                    }
+                    else {
+                        st.pop();
+                    }
+                }
+            }
+            if(st.empty()) {
+                bw.write("YES");
+                bw.newLine();
+            }
+            else {
+                bw.write("NO");
+                bw.newLine();
+            }
+        }
+        br.close();
+        bw.close();
+    }
 
 }
