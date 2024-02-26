f133b04bf60343a6472449f1ebca27f310e8a84c
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-02-26 19:46:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1052 ms, Memory: 170024 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Main{
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int numCount = Integer.parseInt(br.readLine());
+		String[] input_str = br.readLine().split(" ");
+
+		int numCount2 = Integer.parseInt(br.readLine());
+		String[] compare_str = br.readLine().split(" ");
+
+		HashMap<String, Integer> map = new HashMap<>();
+
+		int[] input_num = new int[numCount];
+		int[] compare_num = new int[numCount2];
+		int cnt = 0;
+
+		for (int i = 0; i < numCount; i++) {
+//			input_num[i] = Integer.parseInt(input_str[i]);
+			String key = input_str[i];
+			int value = map.getOrDefault(key, 0);
+			map.put(key, value + 1);
+		}
+		for (int i = 0; i < numCount2; i++) {
+			bw.write(map.getOrDefault(compare_str[i], 0)+" ");
+		}
+
+		br.close();
+		bw.close();
+	}
+}
