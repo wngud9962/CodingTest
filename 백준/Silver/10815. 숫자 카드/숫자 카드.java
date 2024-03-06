a5a7688d7949e492cd2d09941617a3d1efdbc531
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-06 15:44:12 +0900

    [Silver V] Title: 숫자 카드, Time: 1304 ms, Memory: 123216 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] N_num = new int[N];
+		String[] N_str; //= new String[N];
+		
+		N_str = br.readLine().split(" ");
+		
+		for (int i = 0; i < N_str.length; i++) {
+			N_num[i] = Integer.parseInt(N_str[i]); 
+		}
+		
+		int M = Integer.parseInt(br.readLine());
+		
+		int[] M_num = new int[M];
+		String[] M_str = new String[M];
+		
+		M_str = br.readLine().split(" ");
+		
+		for (int i = 0; i < M_str.length; i++) {
+			M_num[i] = Integer.parseInt(M_str[i]); 
+		}
+
+		Arrays.sort(N_num);
+		
+		for (int j = 0; j < M; j++) {
+			if(Arrays.binarySearch(N_num, M_num[j])>=0){
+				bw.write("1 ");
+			}
+			else {
+				bw.write("0 ");
+			}
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
