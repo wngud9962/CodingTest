f59cff6a646c8853a499c7796a32bfeab075c8b1
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-06 16:06:32 +0900

    Update 숫자 카드.java

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -51,3 +51,48 @@ public class Main {
 		bw.close();
 	}
 }
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+//import java.util.Arrays;
+//import java.util.StringTokenizer;
+//
+//public class Ex {
+//	public static void main(String[] args) throws Exception {
+//
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		StringTokenizer st;
+//
+//		int N = Integer.parseInt(br.readLine());
+//		int[] N_num = new int[N];
+//
+//		st = new StringTokenizer(br.readLine());
+//		for (int i = 0; i < N; i++) {
+//			N_num[i] = Integer.parseInt(st.nextToken());
+//		}
+//		
+//		int M = Integer.parseInt(br.readLine());
+//		int[] M_num = new int[M];
+//		
+//		st = new StringTokenizer(br.readLine());
+//		for (int i = 0; i < M_num.length; i++) {
+//			M_num[i]=Integer.parseInt(st.nextToken());
+//		}
+//		
+//		Arrays.sort(N_num);
+//
+//		for (int j = 0; j < M; j++) {
+//			if (Arrays.binarySearch(N_num, M_num[j]) >= 0) {
+//				bw.write("1 ");
+//			} else {
+//				bw.write("0 ");
+//			}
+//		}
+//
+//		br.close();
+//		bw.close();
+//	}
+//}
+
