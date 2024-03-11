1786171ee3fe08ef2e716a0585cebb24ef4d285a
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-11 20:55:32 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int line = Integer.parseInt(br.readLine());
+		
+		String input_str[] = new String[line];
+		
+		for (int i = 0; i < line; i++) {
+			input_str[i] = br.readLine(); // 파일 이름 저장
+		}
+
+		boolean same = true;
+		
+		for (int i = 0; i < input_str[0].length(); i++) {
+			for (int j = 0; j < line; j++) {
+				if(input_str[0].charAt(i) != input_str[j].charAt(i)) {
+					same = false;
+					break;
+				}
+			}
+			
+			if(same) {
+				bw.write(input_str[0].charAt(i));
+			}
+			else {
+				bw.write("?");
+				
+				if (same == true) same = false;
+				else same = true; 
+			}
+		}
+
+		br.close();
+		bw.close();
+	}
+}
