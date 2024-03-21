dd36ffa83b1397b5e048f6097a48cf8547fb08e1
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-21 16:12:59 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 1420 ms, Memory: 296916 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] input = br.readLine().split(" ");
+		long res = 0;
+		
+		for (int i = 0; i < input[0].length(); i++) {
+			for (int j = 0; j < input[1].length(); j++) {
+				res += Integer.parseInt(input[0].charAt(i)+"")*Integer.parseInt(input[1].charAt(j)+"");
+			}
+		}
+		bw.write(res+"");
+
+		br.close();
+		bw.close();
+	}
+}
