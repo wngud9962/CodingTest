325618b3c9e20315cc483a4a4097127f9b2a8898
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-29 02:46:20 +0900

    Update 상수.java

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -31,3 +31,40 @@ public class Main {
 		bw.close();
 	}
 }
+// import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+// import java.io.InputStreamReader;
+// import java.io.OutputStreamWriter;
+// import java.util.StringTokenizer;
+
+// public class Main {
+// 	public static void main(String[] args) throws Exception {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+// 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+// 		int a = Integer.parseInt(st.nextToken());
+// 		int b = Integer.parseInt(st.nextToken());
+
+// 		int rev1 = 0;
+// 		int rev2 = 0;
+
+// 		while (a != 0) {
+// 			rev1 = rev1 * 10 + a % 10;
+// 			a /= 10;
+// 		}
+// 		while (b != 0) {
+// 			rev2 = rev2 * 10 + b % 10;
+// 			b /= 10;
+// 		}
+		
+// 		if(rev1>rev2) {
+// 			bw.write(rev1+"");
+// 		}
+// 		else bw.write(rev2+"");
+
+// 		br.close();
+// 		bw.close();
+// 	}
+// }
