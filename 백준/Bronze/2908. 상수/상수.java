e80da0d433ec23089400ae89eb32ba2cd553b4e6
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-29 02:44:15 +0900

    [Bronze II] Title: 상수, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -12,25 +12,20 @@ public class Main {
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+		String a = st.nextToken();
+		String b = st.nextToken();
 
-		int rev1 = 0;
-		int rev2 = 0;
-
-		while (a != 0) {
-			rev1 = rev1 * 10 + a % 10;
-			a /= 10;
-		}
-		while (b != 0) {
-			rev2 = rev2 * 10 + b % 10;
-			b /= 10;
-		}
+		StringBuffer sb1 = new StringBuffer(a);
+		sb1.reverse();
 		
-		if(rev1>rev2) {
-			bw.write(rev1+"");
-		}
-		else bw.write(rev2+"");
+		StringBuffer sb2 = new StringBuffer(b);
+		sb2.reverse();
+
+		int revA = Integer.parseInt(sb1.toString());
+		int revB = Integer.parseInt(sb2.toString());
+
+		if(revA > revB) bw.write(revA+"");
+		else bw.write(revB+"");
 
 		br.close();
 		bw.close();
