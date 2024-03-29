de78ef19632de5b098c416c719a8a0f2c76520c4
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-29 16:44:51 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,10 +1,13 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -12,22 +15,27 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int line = Integer.parseInt(br.readLine());
-		int input[] = new int[line];
-		
-		Set<Integer> s = new TreeSet<>();
+		Integer input[] = new Integer[line];
 
 		for (int i = 0; i < line; i++) {
 			input[i] = Integer.parseInt(br.readLine());
 		}
 
-//		Arrays.sort(input);
 
+		Set<Integer> s = new HashSet<>();
+		List<Integer> list = Arrays.asList(input);
+		List<Integer> list1 = new ArrayList<>();
+//		int[] resArr = new int[line];
+//		for (int i = 0; i < line; i++) {
+//			s.add(input[i]);
+//		}
+		
+		s.addAll(list);
+		list1.addAll(s);
+		
+		Collections.sort(list1);
 
-		for (int i = 0; i < line; i++) {
-			s.add(input[i]);
-		}
-
-		for (int res : s) {
+		for (int res : list1) {
 			bw.write(res+"");
 			bw.newLine();
 		}
