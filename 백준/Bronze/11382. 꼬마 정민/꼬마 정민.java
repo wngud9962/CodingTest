158499ba9871570a8c431030ae90d7e9f5539c16
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-29 20:37:32 +0900

    Update 꼬마 정민.java

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -21,3 +21,29 @@ public class Main {
 		bw.close();
 	}
 }
+// import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+// import java.io.InputStreamReader;
+// import java.io.OutputStreamWriter;
+// import java.util.StringTokenizer;
+
+// public class Main {
+// 	public static void main(String[] args) throws Exception {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+// 		StringTokenizer st = new StringTokenizer(br.readLine());
+// 		long sum=0;
+
+// 		while (st.hasMoreElements()) {
+// 			long st_num = Long.parseLong((st.nextToken())); 
+// 			sum += st_num;
+
+// 		}
+
+// 		bw.write(sum + "");
+
+// 		br.close();
+// 		bw.close();
+// 	}
+// }
