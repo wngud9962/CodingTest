29759a1bca67161e8dcdb0f0500da02c1de2c51f
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-29 20:36:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,15 +11,11 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		long sum=0;
-
-		while (st.hasMoreElements()) {
-			long st_num = Long.parseLong((st.nextToken())); 
-			sum += st_num;
-
-		}
-
-		bw.write(sum + "");
+		BigInteger numA = new BigInteger(st.nextToken());
+		BigInteger numB = new BigInteger(st.nextToken());
+		BigInteger numC = new BigInteger(st.nextToken());
+		
+		bw.write(numA.add(numB.add(numC))+"");
 
 		br.close();
 		bw.close();
