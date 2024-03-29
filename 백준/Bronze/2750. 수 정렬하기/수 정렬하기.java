05ad0fb02707e16df3758bf62103149aabb1ea67
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-29 17:12:20 +0900

    Update 수 정렬하기.java

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -44,3 +44,41 @@ public class Main {
 		bw.close();
 	}
 }
+
+// 트리셋 사용
+// import java.io.BufferedReader;
+// import java.io.BufferedWriter;
+// import java.io.InputStreamReader;
+// import java.io.OutputStreamWriter;
+// import java.util.Set;
+// import java.util.TreeSet;
+
+// public class Main {
+// 	public static void main(String[] args) throws Exception {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+// 		int line = Integer.parseInt(br.readLine());
+// 		int input[] = new int[line];
+		
+// 		Set<Integer> s = new TreeSet<>();
+
+// 		for (int i = 0; i < line; i++) {
+// 			input[i] = Integer.parseInt(br.readLine());
+// 		}
+
+
+// 		for (int i = 0; i < line; i++) {
+// 			s.add(input[i]);
+// 		}
+
+// 		for (int res : s) {
+// 			bw.write(res+"");
+// 			bw.newLine();
+// 		}
+
+// 		br.close();
+// 		bw.close();
+// 	}
+// }
+
