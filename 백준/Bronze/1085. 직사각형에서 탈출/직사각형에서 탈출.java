6909047ead0b9eaf8f32b2479a0420bb6ed66a39
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-03-29 17:03:27 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 196 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -2,32 +2,43 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.sql.ResultSet;
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
+	
+	public static int min(int...a) {
+		int min =10000000;
+		
+		for(int i: a) {
+			if(i<min) {
+				min=i;
+			}
+		}
+		
+		return min;
+	}
+	
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		Scanner sc = new Scanner(System.in);
 		String input_str[] = br.readLine().split(" ");
 
 		int input_num[] = new int[input_str.length];
+		
 
 		for (int i = 0; i < input_str.length; i++) {
 			input_num[i] = Integer.parseInt(input_str[i]);
 		}
-
-		int x = input_num[0] <= (input_num[2] - input_num[0]) ? input_num[0] : input_num[2] - input_num[0];
-		int y = input_num[1] <= (input_num[3]-input_num[1]) ? input_num[1] : input_num[3]-input_num[1];
+		int x = input_num[0];
+		int y = input_num[1];
+		int w = input_num[2];
+		int h = input_num[3];
+		int min = min(x,y,(w-x),(h-y));
 		
-		int result = x<=y ? x:y;
-		
-		bw.write(result+"");
+		bw.write(min+"");
 
 		br.close();
 		bw.close();
