230a1ef4168325142566fb10f84f4dc56d3cf120
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-04-02 16:02:13 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.print("      |");
+		
+		br.close();
+		bw.close();
+	}
+}
