78752e1ec1e9b2ac0a44219fd100d419ac1aea69
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-04-03 15:02:28 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("92");
+		bw.newLine();
+		bw.write("jh9962");
+		
+		br.close();
+		bw.close();
+	}
+}
