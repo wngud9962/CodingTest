0429133d8922b124ef46fb84a9bbb724cd95f447
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-04-24 15:44:15 +0900

    [Bronze II] Title: 손익분기점, Time: 132 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+import javax.swing.plaf.FontUIResource;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		// A 고정비용
+		// B 하나당 가변비용
+		// C 노트북 가격
+		// (C x 갯수) > A + (B x 갯수) 시점
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cnt = 1;
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		if (B >= C) {
+			bw.write("-1");
+			bw.newLine();
+		} 
+		else {
+			bw.write(A / (C - B) + 1 + "");
+			bw.newLine();
+		}
+
+		br.close();
+		bw.close();
+	}
+}
