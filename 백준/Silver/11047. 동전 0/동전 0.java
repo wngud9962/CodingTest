ec7c99508971c56d09662a5a90cac8ee5e74edc2
wngud9962 <136872374+wngud9962@users.noreply.github.com>
2024-04-25 21:49:26 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14472 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int cnt = 0;
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int line = Integer.parseInt(st.nextToken());
+		int sum = Integer.parseInt(st.nextToken());
+
+		Integer[] input = new Integer[line];
+
+		for (int i = 0; i < input.length; i++) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(input, Comparator.reverseOrder()); // 내림차순
+
+		for (int i = 0; i < input.length; i++) {
+			cnt += (sum / input[i]);
+			sum = sum % input[i];
+		}
+		
+		bw.write(cnt + "");
+
+		br.close();
+		bw.close();
+	}
+}
